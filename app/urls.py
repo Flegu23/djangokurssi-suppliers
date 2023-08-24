2175ea481e22d35be9f5ae7d3bdee223fa8b70aa
Petteri Vuorinen <jaakkopetteri.vuorinen@gmail.com>
2023-08-24 13:03:10 +0300

    first commit

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import landingview, productlistview, supplierlistview
+
+urlpatterns = [
+    path('', landingview),
+
+    path('products/', productlistview),
+
+    path('suppliers/', supplierlistview),
+
+]
